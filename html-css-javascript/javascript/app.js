ceb5a3bd9229050d6a5b6f1883f307bbd6e3f5fa
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-04-06 21:38:06 +0400

    javascript intro

---
+++ html-css-javascript/javascript/app.js
@@ -0,0 +1,33 @@
+// ცვლადის შექმნა
+
+// let/const
+
+let myName = "Coding School v3";
+let message = "Welcome Message";
+
+let year = 3000;
+// console.log(typeof myName);
+// console.log(myName);
+// console.log(message);
+
+// myName = 3000;
+console.log(myName);
+// console.log(year);
+// console.log(typeof myName);
+// console.log(typeof year);
+
+// alert(message);
+const x = 10;
+const y = 20;
+
+const sum = x + y; // alias
+const sub = x - y;
+const mul = x * y;
+const div = x / y;
+const rem = x % y;
+
+console.log("sum", sum);
+console.log("sub", sub);
+console.log("mul", mul);
+console.log("div", div);
+console.log("rem", rem);
