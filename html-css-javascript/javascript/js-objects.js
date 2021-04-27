a4037e4451505c04f53f239547d2a054a767f54d
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-04-27 21:47:50 +0400

    js array object methods

--- html-css-javascript/javascript/js-objects.js
+++ html-css-javascript/javascript/js-objects.js
@@ -52,5 +52,98 @@ const propValue = "country";
 
 // logger(james.firstName);
 // logger(john.firstName);
-logger(User.location.country);
-logger(User[propName][propValue]);
+// logger(User.location.country);
+// logger(User[propName][propValue]);
+
+/**
+ *
+ * @ value type
+ */
+
+// let numberOne = 13;
+// let numberTwo = numberOne;
+// logger(numberTwo === numberOne);
+
+// numberTwo = 200;
+// logger(numberTwo === numberOne);
+
+// let strOne = "String 1";
+// let strTwo = strOne;
+// logger(strOne === strTwo);
+
+// strTwo = "String 2";
+// logger(strOne === strTwo);
+
+/**
+ *
+ * @ reference type
+ */
+// [0, 1, 2, 3, 4]
+const arrOne = ["One", "Two"]; // reference
+const arrTwo = arrOne.slice(); // new reference
+
+// arrTwo.push("Three");
+// logger(arrOne);
+// logger(arrTwo);
+// console.log(arrTwo === arrOne);
+
+// if (arrTwo.includes("Three")) {
+//   logger("Includes Three");
+// }
+
+// const i = arrTwo.indexOf("Three");
+// logger(arrTwo[i]);
+
+// const arrowFunction = (arg1, arg2) => {
+//   console.log(arg1, arg2);
+// };
+
+// arrOne.forEach((value, index) => {
+//   logger(`${index}=${value}`);
+// });
+
+const numbers = [233, 431, 22, 334, 400];
+// const filtered = numbers.filter((value) => {
+//   return value % 2 === 0;
+// });
+// logger(numbers);
+// logger(filtered);
+
+const doubledNumbers = numbers.map((value) => {
+  return value * 2;
+});
+
+// logger(doubledNumbers);
+// Imperative programming
+// Declarative programming
+
+const TodoList = [
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+  {
+    userId: 2,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: false,
+  },
+  {
+    userId: 3,
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false,
+  },
+];
+
+let userId3TodoIndex = TodoList.find((todo) => {
+  return todo.userId === 3;
+});
+
+userId3TodoIndex = TodoList.findIndex((todo) => {
+  return todo.userId === 3;
+});
+
+logger(userId3TodoIndex);
