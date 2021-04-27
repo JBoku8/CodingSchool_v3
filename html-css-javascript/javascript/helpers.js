044bb83242e7f1f2bdd8173dd4e3f2156dd2d103
Jano Bokuchava <jano.bokuchava@btu.edu.ge>
2021-04-27 21:48:43 +0400

    js array object methods (#8)

--- html-css-javascript/javascript/helpers.js
+++ html-css-javascript/javascript/helpers.js
@@ -1,4 +1,9 @@
 function logger(message) {
   console.log("%c-----[logger]-----", "color:red;font-size: 20px");
-  console.log(`%c${message}`, "color:green; font-size: 20px");
+
+  let logText = message;
+  if (typeof message === "object") {
+    logText = JSON.stringify(message, null, 2);
+  }
+  console.log(`%c${logText}`, "color:green; font-size: 20px");
 }
