3764c0ef61a0ee93b8dd4da10442b43c4bcb6de9
Jano Bokuchava <jano.bokuchava@btu.edu.ge>
2021-05-18 21:56:30 +0400

    update: expanse-manager-improvements (#11)

--- html-css-javascript/expanses/js/app.js
+++ html-css-javascript/expanses/js/app.js
@@ -1,3 +1,12 @@
+const parentElement = document.getElementById("expansesList");
+const totalExpanse = document.getElementById("totalExpanse");
+const averageExpanse = document.getElementById("averageExpanse");
+
+const expansesList = [
+  new Expanse("სატესტო ხარჯი 1", 20, "2021-05-02"),
+  new Expanse("სატესტო ხარჯი 2", 36, "2021-05-01"),
+];
+
 function expanseFormSubmit(event) {
   event.preventDefault();
 
@@ -5,17 +14,14 @@ function expanseFormSubmit(event) {
   const expanseAmount = document.getElementById("expanseAmount");
   const expanseDate = document.getElementById("expanseDate");
 
-  //   console.log(expanseTitle.value);
-  //   console.log(+expanseAmount.value);
-  //   console.log(expanseDate.value);
-
-  const newExpanse = Expanse(
+  const newExpanse = new Expanse(
     expanseTitle.value,
     +expanseAmount.value,
     expanseDate.value
   );
 
-  newExpanse.logInfo();
+  expansesList.push(newExpanse);
+  renderExpanses();
 
   expanseTitle.value = "";
   expanseAmount.value = "";
@@ -23,16 +29,71 @@ function expanseFormSubmit(event) {
 }
 
 function Expanse(title, amount, date) {
-  return {
-    title,
-    amount,
-    date,
-    logInfo() {
-      console.group("შენი ხარჯი");
-      console.log(
-        `დასახელება: ${title}. \nთანხა: ${amount} ლარი. \nთარიღი: ${date}. \n`
-      );
-      console.groupEnd();
-    },
-  };
+  this.title = title;
+  this.amount = amount;
+  this.date = date;
+  this.id = Math.random().toString();
 }
+
+Expanse.prototype.logInfo = function () {
+  console.log(this);
+  console.group("შენი ხარჯი");
+  console.log(
+    `დასახელება: ${this.title}. \nთანხა: ${this.amount} ლარი. \nთარიღი: ${this.date}. \n`
+  );
+  console.groupEnd();
+};
+
+function renderExpanses() {
+  parentElement.innerHTML = null;
+  let total = 0;
+  for (let i = expansesList.length - 1; i >= 0; i--) {
+    parentElement.innerHTML += expanseItemTemplate(expansesList[i]);
+    total += expansesList[i].amount;
+  }
+  totalExpanse.textContent = `${total} ლარი.`;
+
+  const average = total ? Math.round(total / expansesList.length) : 0;
+
+  averageExpanse.textContent = `${average} ლარი.`;
+}
+
+function expanseItemTemplate(expanse) {
+  return `
+  <li
+    class="
+      list-group-item
+      d-flex
+      justify-content-between
+      align-items-start
+    "
+    onclick="onExpanseItemClick('${expanse.id}')"
+  >
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">${expanse.title}</div>
+      ${expanse.date}
+    </div>
+    <button class="btn btn-danger btn-sm" onclick="onExpanseItemDelete(event, '${expanse.id}')">
+      <i class="bi bi-trash-fill"></i>
+    </button>
+    <span class="ms-1 badge bg-primary rounded-pill">${expanse.amount} ლარი</span>
+  </li>
+  `;
+}
+
+function onExpanseItemClick(itemId) {
+  const item = expansesList.find((el) => el.id === itemId);
+  console.group("onExpanseItemClick");
+  console.log("expanse item clicked");
+  console.log(item);
+  console.groupEnd();
+}
+
+function onExpanseItemDelete(event, itemId) {
+  event.stopPropagation();
+  const index = expansesList.findIndex((el) => el.id === itemId);
+  expansesList.splice(index, 1);
+  renderExpanses();
+}
+
+renderExpanses();
