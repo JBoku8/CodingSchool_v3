f5e40e4b4a5bb9aa22dc50f15d01829ebc3958cb
Jano Bokuchava <jano.bokuchava@btu.edu.ge>
2021-05-22 13:04:13 +0400

    update: expanse-manager-improvements-part-2 (#12)

--- html-css-javascript/expanses/js/app.js
+++ html-css-javascript/expanses/js/app.js
@@ -1,40 +1,65 @@
 const parentElement = document.getElementById("expansesList");
 const totalExpanse = document.getElementById("totalExpanse");
 const averageExpanse = document.getElementById("averageExpanse");
+const expanseFormContainer = document.getElementById("expanseFormContainer");
+const addNewExpanseBtn = document.getElementById("addNewExpanse");
+const closeFormBtn = document.getElementById("closeFormBtn");
+const submitBtn = document.getElementById("submitBtn");
+
+const expanseTitle = document.getElementById("expanseTitle");
+const expanseAmount = document.getElementById("expanseAmount");
+const expanseDate = document.getElementById("expanseDate");
 
 const expansesList = [
   new Expanse("სატესტო ხარჯი 1", 20, "2021-05-02"),
   new Expanse("სატესტო ხარჯი 2", 36, "2021-05-01"),
 ];
 
+let isEditing = false;
+
+const textModes = {
+  [false]: {
+    submitBtn: "ხარჯის შექმნა",
+  },
+  [true]: {
+    submitBtn: "განახლება",
+  },
+};
+
 function expanseFormSubmit(event) {
   event.preventDefault();
 
-  const expanseTitle = document.getElementById("expanseTitle");
-  const expanseAmount = document.getElementById("expanseAmount");
-  const expanseDate = document.getElementById("expanseDate");
+  if (!isEditing) {
+    const newExpanse = new Expanse(
+      expanseTitle.value,
+      +expanseAmount.value,
+      expanseDate.value
+    );
 
-  const newExpanse = new Expanse(
-    expanseTitle.value,
-    +expanseAmount.value,
-    expanseDate.value
-  );
+    expansesList.push(newExpanse);
+  } else {
+    // რედაქტირება
+    console.log(expanseTitle.value, +expanseAmount.value, expanseDate.value);
+  }
 
-  expansesList.push(newExpanse);
   renderExpanses();
+  emptyForm();
+}
 
+function emptyForm() {
   expanseTitle.value = "";
   expanseAmount.value = "";
   expanseDate.value = "";
-}
 
+  isEditing = false;
+  submitBtn.textContent = textModes[isEditing].submitBtn;
+}
 function Expanse(title, amount, date) {
   this.title = title;
   this.amount = amount;
   this.date = date;
   this.id = Math.random().toString();
 }
-
 Expanse.prototype.logInfo = function () {
   console.log(this);
   console.group("შენი ხარჯი");
@@ -57,7 +82,6 @@ function renderExpanses() {
 
   averageExpanse.textContent = `${average} ლარი.`;
 }
-
 function expanseItemTemplate(expanse) {
   return `
   <li
@@ -65,35 +89,51 @@ function expanseItemTemplate(expanse) {
       list-group-item
       d-flex
       justify-content-between
-      align-items-start
-    "
-    onclick="onExpanseItemClick('${expanse.id}')"
-  >
+      align-items-start">
     <div class="ms-2 me-auto">
       <div class="fw-bold">${expanse.title}</div>
       ${expanse.date}
     </div>
+   <div class="btn-group">
+    <button class="btn btn-warning btn-sm" onclick="onExpanseItemEdit(event, '${expanse.id}')">
+      <i class="bi bi-pencil"></i>
+    </button>
     <button class="btn btn-danger btn-sm" onclick="onExpanseItemDelete(event, '${expanse.id}')">
       <i class="bi bi-trash-fill"></i>
     </button>
+   </div>
     <span class="ms-1 badge bg-primary rounded-pill">${expanse.amount} ლარი</span>
   </li>
   `;
 }
-
-function onExpanseItemClick(itemId) {
-  const item = expansesList.find((el) => el.id === itemId);
-  console.group("onExpanseItemClick");
-  console.log("expanse item clicked");
-  console.log(item);
-  console.groupEnd();
-}
-
 function onExpanseItemDelete(event, itemId) {
   event.stopPropagation();
   const index = expansesList.findIndex((el) => el.id === itemId);
   expansesList.splice(index, 1);
   renderExpanses();
 }
+function onExpanseItemEdit(event, itemId) {
+  event.stopPropagation();
+  const element = expansesList.find((el) => el.id === itemId);
+
+  expanseTitle.value = element.title;
+  expanseAmount.value = element.amount;
+  expanseDate.value = element.date;
+
+  isEditing = true;
+  submitBtn.textContent = textModes[isEditing].submitBtn;
+
+  addNewExpanseBtn.click();
+}
 
 renderExpanses();
+
+addNewExpanseBtn.onclick = function () {
+  expanseFormContainer.removeAttribute("hidden");
+  submitBtn.textContent = textModes[isEditing].submitBtn;
+};
+
+closeFormBtn.onclick = function () {
+  expanseFormContainer.setAttribute("hidden", true);
+  emptyForm();
+};
