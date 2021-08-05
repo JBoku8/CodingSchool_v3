8c6ff1412417c57883fb46371149d9c424e703bc
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-08-05 21:39:47 +0400

    angular 02

---
+++ angular/angular-project/src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  template: `<h2>Hello World Component</h2>`,
+  styleUrls: ['./hello-world.scss'],
+})
+export class HelloWorldComponent {}
