f330b7b1f2c6e1c27bb54a712f02c6b3d88fe683
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-08-10 21:23:51 +0400

    @input decoratot, pipes

---
+++ angular/angular-project/src/app/pipes/exponential-Strength.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'customPipe',
+})
+export class ExponentialStrengthPipe implements PipeTransform {
+  transform(value: number, exponent = 1): number {
+    console.group('ExponentialStrengthPipe');
+    console.log('VALUE', value);
+    console.log('Exponent', exponent);
+    console.groupEnd();
+
+    return value ** exponent;
+  }
+}
